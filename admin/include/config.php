6fdc0b721a74f815aefb1a1c6e184c7afe902a76
ayenibolt <55488000+ayenibolt@users.noreply.github.com>
2020-04-19 00:10:45 +0100

    Update config.php

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'project');
+define('DB_SERVER','eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+define('DB_USER','od81rp7liegjk1gw');
+define('DB_PASS' ,'a2qdjl6p43yi616i');
+define('DB_NAME', 'oi47oh3e2efsjwdb');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
